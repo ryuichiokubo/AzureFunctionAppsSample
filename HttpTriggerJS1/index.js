51761b4372c5dfd146a334cbf3c3cf5c5173f3b3
windowsazure <windowsazure>
2017-04-27 09:41:10 +0000

    Initial Commit

---
+++ HttpTriggerJS1/index.js
@@ -0,0 +1,40 @@
+module.exports = function (context, req) {
+    context.log("HTTP trigger function processed a request. RequestUri=%s", req.originalUrl);
+    context.log("Request Headers = " + JSON.stringify(req.headers));
+    var res;
+
+    if (req.query.name || (req.body && req.body.name)) {
+        if (typeof req.query.name != "undefined") {
+            context.log("Name was provided as a query string param...");
+            res = ProcessNewUserInformation(context, req.query.name);
+        }
+        else {
+            context.log("Processing user info from request body...");
+            res = ProcessNewUserInformation(context, req.body.name, req.body.address);
+        }
+    }
+    else {
+        res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done(null, res);
+};
+
+function ProcessNewUserInformation(context, name, address) {
+    context.log("Processing user information...");
+    context.log("name = " + name);
+    var echoString = "Hello " + name;
+    var res;
+
+    if (typeof address != "undefined") {
+        echoString += "\n" + "The address you provided is " + address;
+        context.log("address = " + address);
+    }
+    res = {
+        // status: 200, /* Defaults to 200 */
+        body: echoString
+    };
+    return res;
+}
